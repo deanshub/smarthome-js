0664b076283e298ef9d07bc1f1323a260a86e77d
Dean Shub <deanshub@gmail.com>
2019-07-20 21:25:00 +0300

    adding lock feature

--- src/commands/windows.js
+++ src/commands/windows.js
@@ -1,5 +1,10 @@
 import { speaker } from 'win-audio'
+import lockSystem from 'lock-system'
 
 export async function mute() {
   return speaker.mute()
+}
+
+export async function lock() {
+  return lockSystem()
 }
