1c3b4b2ca1b831b51c566f94e1d37cc388649083
Dean Shub <deanshub@gmail.com>
2019-07-26 13:26:46 +0300

    volumes

--- src/commands/windows.js
+++ src/commands/windows.js
@@ -1,8 +1,24 @@
 import { speaker } from 'win-audio'
 import lockSystem from 'lock-system'
 
+// speaker.polling(200)
+//
+// let muted = false
+// speaker.events.on('toggle', status => {
+//   muted = status.new
+//   // console.log('muted: %s -> %s', status.old, status.new)
+// })
+
 export async function mute() {
-  return speaker.mute()
+  return speaker.toggle()
+}
+
+export async function volUp() {
+  return speaker.increase(5)
+}
+
+export async function volDown() {
+  return speaker.decrease(5)
 }
 
 export async function lock() {
