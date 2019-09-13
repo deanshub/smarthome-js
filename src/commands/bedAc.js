dd64018551410d19346b77e5e4c2eccc9256ec44
Dean Shub <deanshub@gmail.com>
2019-09-13 09:38:42 +0300

    trying to fix bedroom signals

--- src/commands/bedAc.js
+++ src/commands/bedAc.js
@@ -1,11 +1,19 @@
 import { sendSignal } from '../broadlinkController'
 
 let onSignal = 'bed30.deg'
+let active = false
 export async function off({ device }) {
+  active = false
   await sendSignal({ device, signal: onSignal })
 }
 
 export async function on({ device, signal }) {
   onSignal = signal
-  await sendSignal({ device, signal })
+  if (active && signal === 'bed25.deg') {
+    signal = 'bedTo25.deg'
+  } else if (active && signal === 'bed24.deg') {
+    signal = 'bedTo24.deg'
+  }
+  active = true
+  return sendSignal({ device, signal })
 }
